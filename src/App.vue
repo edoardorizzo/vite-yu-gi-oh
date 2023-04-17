ea8dd827495f51a3fce8133ec815fcd85ce8355c
edoardorizzo <edo96.rizzo@gmail.com>
2023-04-17 17:59:47 +0200

    add ui card list

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,6 @@ export default {
         .then(response => {
           console.log(response);
           this.cards = response.data.data;
-          console.log(this.cards);
         })
     }
   },
@@ -41,8 +40,10 @@ export default {
         <div class="card_image">
           <img :src="card.card_images[0].image_url" :alt="card.name">
         </div>
-        <h4> {{ card.name }} </h4>
-        <div> {{ card.type }} </div>
+        <div class="card_text">
+          <h6> {{ card.name }} </h6>
+          <div> {{ card.type }} </div>
+        </div>
       </div>
     </div>
   </div>
@@ -52,4 +53,38 @@ export default {
 
 <style lang="scss" scoped>
 @use './partials/varibles.scss' as *;
+
+.container {
+  margin: auto;
+
+  .row{
+    justify-content: space-between;
+  }
+}
+
+.card {
+  height: 340px;
+  width: calc(100% / 5);
+  margin: 1rem 1rem;
+  background-color: $opacity-black;
+  color: $white;
+
+  .card_image {
+    margin: 1rem 0;
+
+    img {
+      width: 100%;
+      display: block;
+    }
+  }
+
+  h6{
+    font-size: 16px;
+    font-weight: bold;
+  }
+
+  div{
+    font-size: 14px;
+  }
+}
 </style>
